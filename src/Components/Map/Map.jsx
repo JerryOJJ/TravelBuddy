1b1a469e2e11966625fbf1d0c5fa4e19c3315b71
Jonathan Jeremiah <110501575+JerryOJJ@users.noreply.github.com>
2025-03-13 03:36:14 +0100

    New map code

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,33 +1,34 @@
-import React from "react";
-import "mapbox-gl/dist/mapbox-gl.css";
-import { useState } from "react";
-import ReactMapGl from "react-map-gl/mapbox";
-import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
-import useStyles from "./style";
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const Map = () => {
-  const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)");
-  const [viewPort, setViewPort] = useState({
-    latitude: 28.6448,
-    longitude: 77.216,
-    Zoom: 6,
-  });
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-  return (
-    <div className={classes.mapContainer}>
-      <ReactMapGl
-        {...viewPort}
-        mapboxApiAccessToken="pk.eyJ1IjoiamVycnlvamoiLCJhIjoiY202dHhmcjBhMDh6azJqc2EwcGozcHV1eSJ9.U_LgDSC9Tf1cQumh1YHPIw"
-        width="100%"
-        height="100%"
-        transitionDuration="200"
-        mapStyle="mapbox://styles/jerryojj/cm6u15cyy00dr01se68zp12x2"
-      ></ReactMapGl>
-    </div>
-  );
+const MapboxExample = () => {
+  const mapContainerRef = useRef();
+  const mapRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamVycnlvamoiLCJhIjoiY202dHhmcjBhMDh6azJqc2EwcGozcHV1eSJ9.U_LgDSC9Tf1cQumh1YHPIw';
+
+    mapRef.current = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [12.550343, 55.665957],
+      zoom: 8
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat([12.554729, 55.70651])
+      .addTo(mapRef.current);
+
+    new mapboxgl.Marker({ color: 'black', rotation: 45 })
+      .setLngLat([12.65147, 55.608166])
+      .addTo(mapRef.current);
+
+    return () => mapRef.current.colorremove();
+  }, []);
+
+  return <div id="map" ref={mapContainerRef} style={{ height: '100%' }}></div>;
 };
 
-export default Map;
+export default MapboxExample;
