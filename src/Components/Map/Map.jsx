33190fe5357bb75b7cf9e8d950d871850f0e3032
Jonathan Jeremiah <110501575+JerryOJJ@users.noreply.github.com>
2025-03-15 03:21:50 +0100

    Updated  map code

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,34 +1,28 @@
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
+import useStyles from './style'
 
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-const MapboxExample = () => {
+const Mapbox = () => {
   const mapContainerRef = useRef();
   const mapRef = useRef();
+  const classes = useStyles();
 
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiamVycnlvamoiLCJhIjoiY202dHhmcjBhMDh6azJqc2EwcGozcHV1eSJ9.U_LgDSC9Tf1cQumh1YHPIw';
 
     mapRef.current = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [12.550343, 55.665957],
-      zoom: 8
+      container: mapContainerRef.current,
+      center: [-74.5, 40], 
+      zoom: 9
     });
+  });
 
-    new mapboxgl.Marker()
-      .setLngLat([12.554729, 55.70651])
-      .addTo(mapRef.current);
-
-    new mapboxgl.Marker({ color: 'black', rotation: 45 })
-      .setLngLat([12.65147, 55.608166])
-      .addTo(mapRef.current);
-
-    return () => mapRef.current.colorremove();
-  }, []);
-
-  return <div id="map" ref={mapContainerRef} style={{ height: '100%' }}></div>;
+  return (
+    <div
+      className={classes.mapContainer}
+      ref={mapContainerRef}
+    />
+  );
 };
 
-export default MapboxExample;
+export default Mapbox;
